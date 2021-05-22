a0d24574cf7db6da3356ad4a256cb32372bfa135
AJ <amarjits1@hotmail.com>
2021-05-21 19:16:09 -0600

    wrote some basic programs

--- src/com/another/math/ComputeCircleArea.java
+++ src/com/another/math/ComputeCircleArea.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 public class ComputeCircleArea {
     public static void main(String[] args) {
 
+        final double PI = 3.14159;
+
         Scanner input_obj = new Scanner(System.in);
 
         // Prompting user to input a radius value.
         System.out.print("Enter a value for the radius: ");
         double radius = input_obj.nextDouble();
-        double area = 3.14159 * radius * radius;
+        double area = PI * radius * radius;
 
         System.out.println("The area for a circle of radius " + radius + " is: " + area);
     }
---
+++ src/com/another/math/DisplayTime.java
@@ -0,0 +1,17 @@
+package com.another.math;
+import java.util.Scanner;
+
+public class DisplayTime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter time in seconds: ");
+        int seconds = input.nextInt();
+
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+
+        System.out.println(seconds + " seconds is: " + minutes + " mins & " + remainingSeconds + " secs");
+
+    }
+}
