f232729ef6a54dfbfbff774f8413a71c5c4a9090
AJ <amarjits1@hotmail.com>
2021-06-09 19:56:09 -0600

    getting input from text files

--- src/com/simple_examples/loops/CumulativeSum.java
+++ src/com/simple_examples/loops/CumulativeSum.java
@@ -1,2 +1,18 @@
-package com.simple_examples.loops;public class CumulativeSum {
+package com.simple_examples.loops;
+import java.util.Scanner;
+
+public class CumulativeSum {
+    public static void main(String[] args) {
+
+        int total = 0;
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+        while (num != 0) {
+            total += num;
+            num = input.nextInt();
+        }
+
+        System.out.println("The total sum is: " + total);
+    }
 }
