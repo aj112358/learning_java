0262b818cfda172aacb9547017ae3fa6df4336bd
AJ <amarjits1@hotmail.com>
2021-06-09 19:54:46 -0600

    working with while loops

--- src/com/simple_examples/loops/GuessNumber.java
+++ src/com/simple_examples/loops/GuessNumber.java
@@ -1,2 +1,27 @@
-package com.simple_examples.loops;public class GuessNumber {
+package com.simple_examples.loops;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+
+        int target = (int)(Math.random() * 1001);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter initial guess: ");
+
+        int guess = input.nextInt();
+        while (guess != target) {
+
+            if (guess > target) {
+                System.out.print("Wrong! Guess smaller: ");
+            } else {
+                System.out.print("Wrong! Guess larger: ");
+            }
+
+            guess = input.nextInt();
+        }
+
+        System.out.println("Correct! The number was: " + target);
+
+    }
 }
