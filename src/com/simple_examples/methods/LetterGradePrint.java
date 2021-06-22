a67566c534acd6218c727eac2654a2205ca9e254
AJ <amarjits1@hotmail.com>
2021-06-22 16:09:29 -0600

    void vs. value returning methods

--- src/com/simple_examples/methods/LetterGradePrint.java
+++ src/com/simple_examples/methods/LetterGradePrint.java
@@ -10,6 +10,12 @@ public class LetterGradePrint {
     }
 
     private static void printGrade(double score) {
+
+        if (score < 0 || score > 100) {
+            System.out.println("Invalid score!");
+            return;
+        }
+
         if (score >= 90.0) {
             System.out.println("A");
         } else if (score >= 80.0) {
